6ad82be79b61000863d3a2d8e4ecd598f21b90f3
Raman Tondro <158779818+RMNO21@users.noreply.github.com>
2026-05-05 14:24:57 +0330

    Update IranMirrors.sh

--- IranMirrors.sh
+++ IranMirrors.sh
@@ -195,6 +195,7 @@ backup_debian() {
       cp -a /etc/apt/sources.list.d "$dest/"
     fi
     ok "Backed up APT config to: $dest"
+    ok "Scanning for best mirrors. This may take a while..."
   else
     warn "/etc/apt not found."
   fi
@@ -240,6 +241,7 @@ backup_rhel() {
   if [[ -d /etc/yum.repos.d ]]; then
     cp -a /etc/yum.repos.d "$dest/"
     ok "Backed up YUM/DNF repos to: $dest"
+    ok "Scanning for best mirrors. This may take a while..."
   else
     warn "/etc/yum.repos.d not found."
   fi
@@ -277,6 +279,7 @@ backup_arch() {
   if [[ -f /etc/pacman.d/mirrorlist ]]; then
     cp -a /etc/pacman.d/mirrorlist "$dest/"
     ok "Backed up pacman mirrorlist to: $dest"
+    ok "Scanning for best mirrors. This may take a while..."
   else
     warn "/etc/pacman.d/mirrorlist not found."
   fi
